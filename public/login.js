e48962519c85ac55d65e47c4ad614906ad8f7824
nnccni <admin.seysmo@gmail.com>
2026-04-18 21:33:26 +0500

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,18 +1,18 @@
 document.getElementById("loginForm").addEventListener("submit", async e => {
   e.preventDefault();
-  const login = document.getElementById("login").value;
+  const username = document.getElementById("login").value;
   const password = document.getElementById("password").value;
 
   const res = await fetch("/auth", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ login, password })
+    body: JSON.stringify({ username, password })
   });
   const json = await res.json();
 
   if (json.ok) {
     localStorage.setItem("token", json.token);
-    window.location.href = "/list";
+    window.location.href = "/"; // кабинет открывается по корню
   } else {
     alert("Неверный логин или пароль");
   }
