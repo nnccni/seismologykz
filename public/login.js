49acdcff00644b33f69b89567aef685caff5f510
nnccni <admin.seysmo@gmail.com>
2026-04-18 19:11:27 +0500

    Update login.js

--- public/login.js
+++ public/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginForm").addEventListener("submit", async e => {
   const login = document.getElementById("login").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("https://seismologykz.up.railway.app/auth", {
+  const res = await fetch("/auth", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ login, password })
@@ -12,7 +12,7 @@ document.getElementById("loginForm").addEventListener("submit", async e => {
 
   if (json.ok) {
     localStorage.setItem("token", json.token);
-    window.location.href = "/";
+    window.location.href = "/list";
   } else {
     alert("Неверный логин или пароль");
   }
